a6ca52a266e2a82b4d4c8d03e74999fcf13de132
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2022-06-26 18:00:46 +0300

    swagger ui added project

--- express-starter/index.js
+++ express-starter/index.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const app = express();
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
 const PORT = 8080;
+
+const swaggerOptions = {
+    definition: {
+    info: {
+      title: "My Rest Api",
+      description: "This is my first swagger api",
+      contact: {
+        name: "Amazing Developer",
+      },
+      servers: ["http://localhost:8080"],
+    },
+  },
+  apis: ["index.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+app.use(express.json());
 
 app.listen(PORT, () =>
   console.log(`App is Alive on: http://localhost:${PORT}/`)
 );
-app.use(express.json());
 
 app.get("/tshirt", (req, res) => {
   res.status(200).send({
