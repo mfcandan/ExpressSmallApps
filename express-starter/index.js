0b61c3608f29fe9dd3144fc73c60b04842931b96
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2022-06-26 17:35:17 +0300

    Basic rest api with express.js get,post

--- express-starter/index.js
+++ express-starter/index.js
@@ -1,7 +1,28 @@
-const app = require("express")();
-const PORT = 8080 
+const express = require("express");
+const app = express();
+const PORT = 8080;
 
-app.listen(
-    PORT,
-    () => console.log(`It's alive: ${PORT}`)
+app.listen(PORT, () =>
+  console.log(`App is Alive on: http://localhost:${PORT}/`)
 );
+app.use(express.json());
+
+app.get("/tshirt", (req, res) => {
+  res.status(200).send({
+    tshirt: "👕",
+    size: "large",
+  });
+});
+
+app.post("/tshirt/:id", (req, res) => {
+  const { id } = req.params;
+  const { logo } = req.body;
+
+  if (!logo) {
+    res.status(418).send({ message: "We need logo!" });
+  }
+
+  res.send({
+    tshirt: `👕 with your ${logo} and ID of ${id}`,
+  });
+});
